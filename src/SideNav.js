bf147611a0dc2ba14ae1152bedbad05c6637d744
Jordan <jordanleelong5@gmail.com>
2020-12-18 17:31:28 -0800

    added basic nav style

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,1 +1,20 @@
 import React from 'react'
+// import './nav.css'
+export default function SideNav() {
+    return (
+
+        <div className='nav-container'>
+
+            <div className='nav-section'>
+            <ul className='ul-for-nav'>
+                <li>Home</li>
+                <li>Explore</li>
+                <li>Messages</li>
+                <li>Profile</li>
+                <button>Tweet</button>
+            </ul>
+
+            </div>
+        </div>
+        )
+}
